a1e226220c0ec3f7f631c0bddd67f95e8210df17
guohaohao <guohaohao@baidu.com>
2018-11-03 15:18:38 +0800

    Directly Set Unit Size Now

--- src/entity/NetworkStructure.py
+++ src/entity/NetworkStructure.py
@@ -48,13 +48,28 @@ class NetworkStructureHandler(object):
             if self.character_length <= 0:
                 raise Exception("param network.character_length should be positive!")
 
+            # self.unit_size
+            try:
+                self.unit_size = float(conf.get("network", "unitSize"))
+                if self.unit_size <= 0:
+                    raise Exception("param network.unitSize should be positive!")
+            except configparser.NoOptionError:
+                logging.info("[INFO] unitSize params not detect!")
+                self.unit_size = 0
+
             # self.radius_params
-            self.__radius_params_str = conf.get("network", "radiusParams").split(",")
-            self.radius_params = [float(x.strip()) for x in self.__radius_params_str]
-            if len(self.radius_params) != 2:
-                raise Exception("param network.radiusParams should have length 2!")
-            if min(self.radius_params) <= 0:
-                raise Exception("param network.radius_params should be positive!")
+            try:
+                self.__radius_params_str = conf.get("network", "radiusParams").split(",")
+                self.radius_params = [float(x.strip()) for x in self.__radius_params_str]
+                if len(self.radius_params) != 2:
+                    raise Exception("param network.radiusParams should have length 2!")
+                if min(self.radius_params) <= 0:
+                    raise Exception("param network.radius_params should be positive!")
+            except configparser.NoOptionError:
+                if self.unit_size != 0:
+                    self.radius_params = list()
+                else:
+                    raise Exception("param network.unitSize and network.radiusParams should at least set one!")
 
             # self.curvature
             try:
@@ -62,7 +77,7 @@ class NetworkStructureHandler(object):
                 if self.curvature <= 0:
                     raise Exception("param network.curvature should be positive!")
             except configparser.NoOptionError:
-                logging.info("Curvature params not detect!")
+                logging.info("[INFO] Curvature params not detect!")
                 self.curvature = 0
 
             # self.throat_params
@@ -120,8 +135,11 @@ class NetworkStructureHandler(object):
         logging.info("模型尺寸: " + str(self.model_size[0]) + " × " + str(self.model_size[1]) + \
                      " × " + str(self.model_size[2]))
         logging.info("特征尺度(m): " + str(self.character_length))
-        logging.info("孔隙半径参数(lc):")
-        logging.info("    均值 = " + str(self.radius_params[0]) + ", 标准差 = " + str(self.radius_params[1]))
+        if self.unit_size != 0:
+            logging.info("单元尺寸(lc): " + str(self.unit_size))
+        else:
+            logging.info("孔隙半径参数(lc):")
+            logging.info("    均值 = " + str(self.radius_params[0]) + ", 标准差 = " + str(self.radius_params[1]))
         if self.curvature != 0:
             logging.info("孔喉曲率(lc): " + str(self.curvature))
         else:
@@ -148,6 +166,7 @@ class NetworkStructure(object):
         self.throatR = None
         self.weight = None
         self.unit_size = None
+        self.porosity = None
         self.initialize()
 
     def initialize(self):
@@ -157,13 +176,19 @@ class NetworkStructure(object):
         """
         logging.info("------------------初始化网络结构------------------")
 
+        if self.nc.unit_size == 0:
+            logging.info("计算单元尺寸……")
+            self.unit_size = self.calculate_unit_size()
+            logging.info("    单元尺寸(lc): " + format(self.unit_size, '.4f'))
+        else:
+            self.unit_size = self.nc.unit_size
+            logging.info("计算设定孔隙尺寸……")
+            self.nc.radius_params = self.calculate_pore_size()
+            logging.info("    设定孔隙尺寸(lc): " + format(self.nc.radius_params[0], '.4f'))
+
         logging.info("初始化孔隙尺寸……")
         self.radii = Tools.Tools.create_normal_dist(self.nc.radius_params[0], self.nc.radius_params[1], self.model_size)
         logging.info("    平均孔隙尺寸(lc): " + format(np.average(self.radii), '.4f'))
-
-        logging.info("计算单元尺寸……")
-        self.unit_size = self.calculate_unit_size()
-        logging.info("    单元尺寸(lc): " + format(self.unit_size, '.4f'))
 
         logging.info("初始化喉道尺寸……")
         if self.nc.curvature:
@@ -177,14 +202,19 @@ class NetworkStructure(object):
         target_coor = Tools.Tools.create_normal_dist(self.nc.coor_params[0], self.nc.coor_params[1], self.model_size)
         self.weight = self.create_weight_data(target_coor)
         logging.info("    平均配位数: " + format(np.average(np.sum(self.weight[1:-1, 1:-1, 1:-1, :], 3)), '.4f'))
-        logging.info("    平均权重:" + format(np.average(self.weight[1:-1, 1:-1, 1:-1, :]), '.4f'))
+        logging.info("    平均权重: " + format(np.average(self.weight[1:-1, 1:-1, 1:-1, :]), '.4f'))
+
+        logging.info("计算网络最终孔隙率……")
+        self.porosity = self.calculate_final_porosity()
+        logging.info("    孔隙率: " + format(self.porosity, '.4f'))
 
     def calculate_unit_size(self):
         """
         迭代法计算单元尺寸
         :return:
         """
-        pore_volumn = 4.0 / 3.0 * np.pi * np.average(np.power(self.radii, 3))
+        # E(X^3) = μ^3 + 3μσ^2, if X ~ N(μ, σ)
+        pore_volumn = 4.0 / 3.0 * np.pi * ((self.nc.radius_params[0] ** 3) + 3 * self.nc.radius_params[0] * (self.nc.radius_params[1] ** 2))
         unit_size = np.power(pore_volumn / self.nc.porosity, 1.0/3.0)
         lo = unit_size
         hi = unit_size * 2.0
@@ -209,6 +239,58 @@ class NetworkStructure(object):
             else:
                 hi = unit_size
         return unit_size
+
+    def calculate_pore_size(self):
+        """
+        迭代法计算孔隙尺寸
+        :return:
+        """
+        var_ratio = 0.2
+        pore_radius = np.power((self.unit_size ** 3) * self.nc.porosity * 3.0 / 4.0 / np.pi / (1 + 3 * (var_ratio ** 2)), 1.0/3.0)
+        hi = pore_radius
+        lo = 0
+        iter_count = 0
+        while np.abs(hi - lo) > 1:
+            iter_count += 1
+            if iter_count > 100:
+                raise Exception("Solve pore size error: iteration more than 100!")
+            pore_radius = (lo + hi) / 2
+            pore_volumn = 4.0 / 3.0 * np.pi * (1 + 3 * (var_ratio ** 2)) * (pore_radius ** 3)
+            if self.nc.curvature:
+                # 使用统计结论：在合理范围内，取r=μ-0.3σ得到的结果较准
+                eff_r = (1 - 0.3 * var_ratio) * pore_radius
+                ave_sqr = np.square(self.cal_throat_radius(eff_r, eff_r, self.unit_size, self.nc.curvature))
+            else:
+                # E(X^2) = (E(X))^2 + D(X)
+                ave_sqr = np.square(self.nc.throat_params[0]) + np.square(self.nc.throat_params[1])
+            throat_length = self.unit_size - 2 * pore_radius
+            throat_volumn = np.pi * ave_sqr * self.nc.coor_params[0] * throat_length / 2.0
+            error = (throat_volumn + pore_volumn) / np.power(self.unit_size, 3.0) - self.nc.porosity
+            if error > 0:
+                hi = pore_radius
+            else:
+                lo = pore_radius
+        return [pore_radius, var_ratio * pore_radius]
+
+    def calculate_final_porosity(self):
+        """
+        计算网络最终孔隙率
+        :return:
+        """
+        pore_volumn = 4.0 / 3.0 * np.pi * np.average(np.power(self.radii, 3.0))
+        throat_volumn = 0
+        for i in range(1, self.model_size[0] - 1):
+            for j in range(1, self.model_size[1] - 1):
+                for k in range(1, self.model_size[2] - 1):
+                    ind1 = (i, j, k)
+                    for l in range(26):
+                        rp = Tools.Tools.get_relative_position(l)
+                        ind2 = tuple(np.add(rp, (i, j, k)))
+                        throat_volumn += np.pi * (self.throatR[i, j, k, l] ** 2) \
+                                         * (self.unit_size - self.radii[ind1] - self.radii[ind2]) \
+                                         * self.weight[i, j, k, l]
+        throat_volumn /= 2 * (self.model_size[0] - 2) * (self.model_size[1] - 2) * (self.model_size[2] - 2)
+        return (pore_volumn + throat_volumn) / (self.unit_size ** 3)
 
     def create_throat_from_curvature(self, curv, unit_size):
         """
